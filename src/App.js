40dee8573d7ba39501c7706cbf9f5825a5f80523
Rohan Patil <rohanpatil5122001@gmail.com>
2024-04-17 11:46:14 +0530

    Hotel menu and order project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import "./style.css";
+
+import Home from "./Food delivery/Components/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
